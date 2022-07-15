52c5a734b11cd9a371f5e83d36cbcc8926fa70b8
shivwp <wpexpert0141@gmail.com>
2022-07-15 09:49:26 +0530

    morningupdate15july

--- app/Http/Controllers/frontend/FrontendController.php
+++ app/Http/Controllers/frontend/FrontendController.php
@@ -15,11 +15,11 @@ class FrontendController extends Controller
 
         $d['online']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->where('users.is_online','=',1)->get();
         $d['new']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->take(4)->get();        
-        $d['phone']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.phone','=',1)->take(4)->get();
+        $d['phone']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.phone','=',1)->get();
         $d['video']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.video','=',1)->take(4)->get();
         $d['featured']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.featured','=',1)->get();
         $d['trending']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.trending','=',1)->take(4)->get();
-        $d['explore']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.explore','=',1)->take(4)->get();
+        $d['explore']=Models::join('users','users.id','=','models.user_id')->select('models.*','users.*')->orderBy('users.created_at','asc')->where('models.explore','=',1)->take(10)->get();
         return view('frontend.home',$d);
     }
 }
